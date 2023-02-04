05d2cc86e007c948718be6ff266bee81227873e1
wn75588 <124462073+wn75588@users.noreply.github.com>
2023-02-04 23:54:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,4 +19,4 @@ fruits_selected = treamlit.multiselect("Pick some fruits:", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display table 
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show);
