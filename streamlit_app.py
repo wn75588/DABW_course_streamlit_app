82c5144a3e64089ce053a7da79aacab750501ced
wn75588 <124462073+wn75588@users.noreply.github.com>
2023-02-04 21:12:45 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import streamlit
 streamlit.title('My Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('\N{apple}')
+streamlit.text('\N{U+1F34E}')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
