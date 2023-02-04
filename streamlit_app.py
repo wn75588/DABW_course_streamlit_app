7786defa1a51942720b9b47202c947a2b4643dcc
wn75588 <124462073+wn75588@users.noreply.github.com>
2023-02-04 23:45:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.text('\N{egg} Hard-Boiled Free-Range Egg')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 streamlit.dataframe(my_fruit_list)
 
 # a pick list
